49bb8563bdf6f0f11035f3f154a2288dbbf79489
Matan Morse <matanbryant@gmail.com>
2024-10-19 10:58:50 -0400

    use icalendar, open ics files

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import *
+import icalendar
+from pathlib import Path
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     
-    return "Hello, world!"
-
+    with open('matan_classes.ics', 'rb') as ics_file:
+        calendar = icalendar.Calendar.from_ical(ics_file.read())
+        print(calendar)
+    return "hi"
 if __name__ == "__main__":
     app.run(debug=True)
